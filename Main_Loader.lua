26df472fa9abf8958aeb4926eeea0da1bdca8e10
Prodgc <162835899+Prodgc@users.noreply.github.com>
2025-06-14 21:56:15 +0300

    Update Main_Loader.lua

--- Main_Loader.lua
+++ Main_Loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Prodgc/Roblox-Scripts/refs/heads/main/Loader.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Prodgc/Roblox-Scripts/refs/heads/main/Discord_Joiner.lua", true))()
